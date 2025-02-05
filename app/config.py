e8c7ae753ab74b70ba99f3356449b53091eb1a44
brankodra <dragosavljevibr77238@th-nuernberg.de>
2025-02-05 19:53:06 +0100

    updated config.py

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "secret_key_example")
     ALGORITHM = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
-    MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+    MONGODB_URL = os.getenv("MONGODB_URL")
